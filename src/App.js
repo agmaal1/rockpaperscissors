8c69c991f7afc01f23a56a136db0515bcb198b4e
Agustin M <agmaal1@gmail.com>
2020-08-12 12:51:37 +0100

    added html structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <section className="main">
+      <div className="content">
+        <div className="box">
+          <div className="header-row">
+            <header>
+              <h2>Rock Paper Scissors</h2>
+            </header>
+          </div>
+          <div className="players-row">
+            <div className="player">player 1</div>
+            <div className="result">
+              <h3>VS</h3>
+            </div>
+            <div className="player">player 2</div>
+          </div>
+          <div className="btn-row">
+            <button className='btn' >Rock</button>
+            <button className='btn' >Paper</button>
+            <button className='btn' >Scissors</button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
